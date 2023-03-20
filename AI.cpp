3d1def7d86713b3a1a6c4b5b688725802743cb84
SherlockAlex <101167329+SherlockAlex@users.noreply.github.com>
2023-03-20 23:20:08 +0800

    更新

--- AI.cpp
+++ AI.cpp
@@ -3,9 +3,12 @@
 int learnCount = 10;//学习十次
 
 double xData[10] = {0.1,0.2,0.3,0.1,0.5,0.0,0.7,0.8,0.9};
-double yData[10];
+double yData[1] = {0.5};
 
+//训练模型,dim(x)=dim(w),dim(b)=神经元个数
+double model(double * x,double * w,double b);
 
+double loss(double y,double t);
 
 void start()
 {
@@ -23,4 +26,21 @@ double ReLU(double x)
 		return 0.0;
 	}
 	return x;
+}
+
+double model(double* x, double* w, double b)
+{
+	//z=wx+b
+	float z=0;
+
+	for (int i = 0; i < 10; i++) {
+		z += x[i] * w[i];
+	}
+	z += b;
+	return z;
+}
+
+double loss(double y, double t)
+{
+	return 0.0;
 }
