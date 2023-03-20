1d9bb267425da13be0b3a89e1e72b5fafe301f1f
SherlockAlex <101167329+SherlockAlex@users.noreply.github.com>
2023-03-20 23:47:33 +0800

    更新

--- AI.cpp
+++ AI.cpp
@@ -1,11 +1,13 @@
 #include "AI.h"
 
-int learnCount = 10;//学习十次
+int learnCount = 1000;//学习次数
 
 double w[2] = { 0.3, 0.4 };
 double xData[2] = { 0.2,0.4 };
 double t[1] = { 0.5 };
 double b = 0.45;
+
+static int step=0;
 
 void grad(double y);//计算梯度
 
@@ -19,8 +21,10 @@ void learnLoop()
 	//学习
 	double z = w[0] * xData[0] + w[1] * xData[1] + b;
 	double y = ReLU(z);
-
-
+	grad(y);
+	cout <<"学习次数" << ++step << endl;
+	cout << "训练模型:"<<"y=("<<w[0]<<"X1)+("<<w[1]<<"X2)+"<<b << endl;
+	cout <<"测试输出y="<< y << endl<<endl;
 }
 
 double ReLU(double x)
@@ -31,7 +35,7 @@ double ReLU(double x)
 	return x;
 }
 
-void grad(double z,double y)
+void grad(double y)
 {
 	b = b - 0.01 * (y - t[0]);
 	w[0] = w[0] - 0.01 * (y - t[0]) * xData[0];
