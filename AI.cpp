714eb8052f3878e5ac7aedd20cf34c69ad4d9f6a
SherlockAlex <101167329+SherlockAlex@users.noreply.github.com>
2023-03-20 23:54:32 +0800

    更新

--- AI.cpp
+++ AI.cpp
@@ -19,11 +19,18 @@ void start()
 void learnLoop()
 {
 	//学习
+	step++;
+
 	double z = w[0] * xData[0] + w[1] * xData[1] + b;
 	double y = ReLU(z);
 	grad(y);
-	cout <<"学习次数" << ++step << endl;
+	if (step%20!=0) {
+		//每50次输出一次
+		return;
+	}
+	cout <<"学习次数:" << step << endl;
 	cout << "训练模型:"<<"y=("<<w[0]<<"X1)+("<<w[1]<<"X2)+"<<b << endl;
+	cout << "loss=" << 0.5 * (y - t[0]) * (y - t[0]) << endl;
 	cout <<"测试输出y="<< y << endl<<endl;
 }
 
